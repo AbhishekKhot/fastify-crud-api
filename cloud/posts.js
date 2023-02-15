eb9188cf45c6e02ebb7123bad0f5858582da6be1
abhishekkhot <khotabhishek469@gmail.com>
2023-02-15 11:02:32 +0530

    initial commit

---
+++ cloud/posts.js
@@ -0,0 +1,9 @@
+const posts = [
+  { id: 1, title: "Post One", body: "This is post one" },
+  { id: 2, title: "Post Two", body: "This is post two" },
+  { id: 3, title: "Post Three", body: "This is post three" }, 
+  { id: 4, title: "Post Four", body: "This is post four" }, 
+  { id: 5, title: "Post Five", body: "This is post five" }, 
+];
+
+module.exports = posts;
